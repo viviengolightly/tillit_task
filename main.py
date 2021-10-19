0bb855697f16f4812ede543722268cbf48b90533
viviengolightly <marta_szukalska@wp.pl>
2021-10-19 11:39:41 +0100

    Update main

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+import argparse
 from src.crawler import Crawler
 
 def main():
-	url = "https://scrapethissite.com"
-	crawler = Crawler(url)
-	crawler.process()
-	
+	parser = argparse.ArgumentParser(description="Usage: main.py --url url_to_crawl --num optional_num_of_workers")
+	parser.add_argument("--url")
+	parser.add_argument("--num")
+	args = parser.parse_args()
+	if args.url:
+		if args.num:
+			crawler = Crawler(args.url, args.num)
+			crawler.process()
+		else:
+			crawler = Crawler(args.url)
+			crawler.process()
+	else:
+		print("Url is required.")
+
 if __name__ == "__main__":
     main()
